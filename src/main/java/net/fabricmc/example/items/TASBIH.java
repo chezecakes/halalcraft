727639142fc7444a35c6271bc11b0acbbc6566e2
o_0 <sircheze6115@gmail.com>
2023-04-28 01:16:05 -0400

    first commit
    commit

---
+++ src/main/java/net/fabricmc/example/items/TASBIH.java
@@ -0,0 +1,9 @@
+package net.fabricmc.example.items;
+
+import net.minecraft.item.Item;
+
+public class TASBIH extends Item {
+    public TASBIH(Settings settings) {
+        super(settings);
+    }
+}

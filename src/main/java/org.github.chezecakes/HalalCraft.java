10bcaef18521c17550b5382fa286b3560ebbef68
Ali Rizvi <perm-iterate-0b@icloud.com>
2023-04-28 23:51:02 -0400

    Add creative tab item group

--- src/main/java/org.github.chezecakes/HalalCraft.java
+++ src/main/java/org.github.chezecakes/HalalCraft.java
@@ -3,6 +3,7 @@ package org.github.chezecakes;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -17,17 +18,19 @@ import org.slf4j.LoggerFactory;
 public class HalalCraft implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("halalcraft");
 
-	public static final Item TASBIH = new TasbihItem(new FabricItemSettings());
-	public static final Item SAJDEGAH = new SajdegahItem(new FabricItemSettings());
+	public static final Item TASBIH = Registry.register(Registries.ITEM, new Identifier("halalcraft", "tasbih"), new TasbihItem(new FabricItemSettings()));
+	public static final Item SAJDEGAH = Registry.register(Registries.ITEM, new Identifier("halalcraft", "sajdegah"), new SajdegahItem(new FabricItemSettings()));
 
-	private static final ItemGroup HALALCRAFT_GROUP = FabricItemGroup.builder(new Identifier("halalcraft", "Halalcraft"))
+	private static final ItemGroup HALALCRAFT_GROUP = FabricItemGroup.builder(new Identifier("halalcraft", "halalcraft"))
 			.icon(() -> new ItemStack(TASBIH))
 			.build();
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Registering items..");
 
-		Registry.register(Registries.ITEM, new Identifier("halalcraft", "tasbih"), TASBIH);
-		Registry.register(Registries.ITEM, new Identifier("halalcraft", "sajdegah"), SAJDEGAH);
+		ItemGroupEvents.modifyEntriesEvent(HALALCRAFT_GROUP).register(content -> {
+			content.add(TASBIH);
+			content.add(SAJDEGAH);
+		});
 	}
 }

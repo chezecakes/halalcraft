804bf7afc9b46d7a6b560f9f04ec24596222ff4d
o_0 <sircheze6115@gmail.com>
2023-04-28 23:40:30 -0400

    added item group
    it broke runclient

--- src/main/java/org.github.chezecakes/HalalCraft.java
+++ src/main/java/org.github.chezecakes/HalalCraft.java
@@ -2,7 +2,10 @@ package org.github.chezecakes;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -17,6 +20,9 @@ public class HalalCraft implements ModInitializer {
 	public static final Item TASBIH = new TasbihItem(new FabricItemSettings());
 	public static final Item SAJDEGAH = new SajdegahItem(new FabricItemSettings());
 
+	private static final ItemGroup HALALCRAFT_GROUP = FabricItemGroup.builder(new Identifier("halalcraft", "Halalcraft"))
+			.icon(() -> new ItemStack(TASBIH))
+			.build();
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Registering items..");

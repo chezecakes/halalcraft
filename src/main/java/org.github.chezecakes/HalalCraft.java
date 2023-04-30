8afaec258eb379860415c176c95244a17999fda4
o_0 <sircheze6115@gmail.com>
2023-04-30 00:29:00 -0400

    added tasbih variants

--- src/main/java/org.github.chezecakes/HalalCraft.java
+++ src/main/java/org.github.chezecakes/HalalCraft.java
@@ -15,22 +15,30 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import org.github.chezecakes.blocks.LimestoneBlock;
 import org.github.chezecakes.blocks.MarbleBlock;
-import org.github.chezecakes.items.KhaakBeadsItem;
-import org.github.chezecakes.items.KhaakItem;
-import org.github.chezecakes.items.SajdegahItem;
-import org.github.chezecakes.items.TasbihItem;
+import org.github.chezecakes.items.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/*
+todo:
+- add a way to templock all tasbih variants when a user uses one tasbih
+
+*/
 public class HalalCraft implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("halalcraft");
 
-    private static final Item KHAAK, TASBIH, SAJDEGAH, KHAAK_BEADS, LIMESTONE, MARBLE;
+    private static final Item KHAAK, GOLD_TASBIH, DIAMOND_TASBIH, EMERALD_TASBIH, IRON_TASBIH, QUARTZ_TASBIH, AMETHYST_TASBIH, NETHER_STAR_TASBIH, SAJDEGAH, KHAAK_BEADS, LIMESTONE, MARBLE;
     private static final ItemGroup HALALCRAFT_GROUP;
 
     static {
         KHAAK = Registry.register(Registries.ITEM, new Identifier("halalcraft", "khaak"), new KhaakItem(new FabricItemSettings()));
-        TASBIH = Registry.register(Registries.ITEM, new Identifier("halalcraft", "tasbih"), new TasbihItem(new FabricItemSettings()));
+        IRON_TASBIH = Registry.register(Registries.ITEM, new Identifier("halalcraft", "iron_tasbih"), new IronTasbihItem(new FabricItemSettings()));
+        GOLD_TASBIH = Registry.register(Registries.ITEM, new Identifier("halalcraft", "gold_tasbih"), new GoldTasbihItem(new FabricItemSettings()));
+        DIAMOND_TASBIH = Registry.register(Registries.ITEM, new Identifier("halalcraft", "diamond_tasbih"), new DiamondTasbihItem(new FabricItemSettings()));
+        EMERALD_TASBIH = Registry.register(Registries.ITEM, new Identifier("halalcraft", "emerald_tasbih"), new EmeraldTasbihItem(new FabricItemSettings()));
+        QUARTZ_TASBIH = Registry.register(Registries.ITEM, new Identifier("halalcraft", "quartz_tasbih"), new QuartzTasbihItem(new FabricItemSettings()));
+        AMETHYST_TASBIH = Registry.register(Registries.ITEM, new Identifier("halalcraft", "amethyst_tasbih"), new AmethystTasbihItem(new FabricItemSettings()));
+        NETHER_STAR_TASBIH = Registry.register(Registries.ITEM, new Identifier("halalcraft", "nether_star_tasbih"), new NetherStarTasbihItem(new FabricItemSettings()));
         SAJDEGAH = Registry.register(Registries.ITEM, new Identifier("halalcraft", "sajdegah"), new SajdegahItem(new FabricItemSettings()));
         KHAAK_BEADS = Registry.register(Registries.ITEM, new Identifier("halalcraft", "khaak_beads"), new KhaakBeadsItem(new FabricItemSettings()));
 
@@ -49,7 +57,7 @@ public class HalalCraft implements ModInitializer {
         MARBLE = Registry.register(Registries.ITEM, marble_id, marble_block);
 
         HALALCRAFT_GROUP = FabricItemGroup.builder(new Identifier("halalcraft", "halalcraft"))
-                .icon(() -> new ItemStack(TASBIH))
+                .icon(() -> new ItemStack(GOLD_TASBIH))
                 .build();
     }
 
@@ -59,7 +67,13 @@ public class HalalCraft implements ModInitializer {
 
         ItemGroupEvents.modifyEntriesEvent(HALALCRAFT_GROUP).register(content -> {
             content.add(KHAAK);
-            content.add(TASBIH);
+            content.add(IRON_TASBIH);
+            content.add(GOLD_TASBIH);
+            content.add(DIAMOND_TASBIH);
+            content.add(EMERALD_TASBIH);
+            content.add(QUARTZ_TASBIH);
+            content.add(AMETHYST_TASBIH);
+            content.add(NETHER_STAR_TASBIH);
             content.add(SAJDEGAH);
             content.add(KHAAK_BEADS);
             content.add(LIMESTONE);
